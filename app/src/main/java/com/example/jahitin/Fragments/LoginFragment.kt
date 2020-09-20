148142f6985f4444afb6b25650207eba03006a9b
Ghalistan <gghalistan@gmail.com>
2020-09-20 13:38:16 +0700

    Authentication Page - Update Layout for better ux

--- app/src/main/java/com/example/jahitin/Fragments/LoginFragment.kt
+++ app/src/main/java/com/example/jahitin/Fragments/LoginFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import com.example.jahitin.R
 
 class LoginFragment : Fragment() {
+    private lateinit var buttonLogin : Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,5 +18,13 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        buttonLogin = view.findViewById(R.id.buttonLogin)
+        buttonLogin.setOnClickListener {
+            Toast.makeText(activity, "LOGIN", Toast.LENGTH_SHORT).show()
+            activity!!.finish()
+        }
     }
 }
--- app/src/main/java/com/example/jahitin/Fragments/RegisterFragment.kt
+++ app/src/main/java/com/example/jahitin/Fragments/RegisterFragment.kt
@@ -1,13 +1,26 @@
 package com.example.jahitin.Fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.jahitin.AuthenticationActivity
 import com.example.jahitin.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-class RegisterFragment : Fragment() {
+class RegisterFragment : Fragment(), View.OnClickListener {
+    private lateinit var auth : FirebaseAuth
+    private lateinit var nama : EditText
+    private lateinit var email : EditText
+    private lateinit var password : EditText
+    private lateinit var buttonRegis : Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,5 +28,55 @@ class RegisterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        auth = Firebase.auth
+        nama = view.findViewById(R.id.inputNamaLengkap)
+        email = view.findViewById(R.id.inputEmail)
+        password = view.findViewById(R.id.inputPassword)
+        buttonRegis = view.findViewById(R.id.buttonRegister)
+
+        buttonRegis.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.buttonRegister -> {
+                if (!cekEmpty()) {
+                    Log.d("Pesan", "GA KOSONG")
+                }
+            }
+        }
+    }
+
+    fun cekEmpty() : Boolean {
+        return if (nama.text.isEmpty()) {
+            nama.error = "Masukkan nama lengkap anda"
+            true
+        }
+        else if (email.text.isEmpty()) {
+            email.error = "Masukkan email anda"
+            true
+        }
+        else if (password.text.isEmpty() || password.text.length < 6) {
+            password.error = "Password tidak boleh kosong atau kurang dari 6 huruf"
+            true
+        } else {
+            false
+        }
+    }
+
+    fun registAccount(email : String, pass : String) {
+        auth.createUserWithEmailAndPassword(email, pass)
+            .addOnCompleteListener() { task ->
+                if (task.isSuccessful) {
+                    Log.d("Pesan", "registerberhasil")
+                } else {
+                    Log.w("Pesan", "failure", task.exception)
+                }
+            }
     }
 }
